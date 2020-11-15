2dee9d3f9a70d6003845d8fa63f4218b0b89617c
Pranav Manral <pranavmanral123@gmail.com>
2020-11-15 15:29:30 +1100

    Prototypical Avatar Customization

--- New Unity Project/Assets/Scripts/ChangingColour.cs
+++ New Unity Project/Assets/Scripts/ChangingColour.cs
@@ -1,10 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ChangingColour : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public SpriteRenderer head;
+
+    public Color pink;
+    public Color blue;
+    public Color green;
+    //public Image displayCopy;
+    public int whatColor = 1;
+
+
+
+    public SpriteRenderer torso;
+    public Color yellow;
+    public Color lightPink;
+    public Color skin;
+
+    public int torsoColor = 1;
+
+
+
+
     void Start()
     {
         
@@ -13,6 +33,65 @@ public class ChangingColour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //displayCopy = head.color;
+
+        if(whatColor == 1)
+        {
+            head.color = pink;
+        }
+        else if(whatColor == 2)
+        {
+            head.color = blue;
+        }
+        else if (whatColor == 3)
+        {
+            head.color = green;
+        }
+
+
+
+        if (torsoColor == 1)
+        {
+            torso.color = yellow;
+        }
+        else if (torsoColor == 2)
+        {
+            torso.color = lightPink;
+        }
+        else if (torsoColor == 3)
+        {
+            torso.color = skin;
+        }
+    }
+
+    public void ChangeHeadPink()
+    {
+        whatColor = 1;
+    }
+
+    public void ChangeHeadBlue()
+    {
+        whatColor = 2;
+    }
+
+    public void ChangeHeadGreen()
+    {
+        whatColor = 3;
+    }
+
+
+    public void ChangeTorsoYellow()
+    {
+        torsoColor = 1;
+    }
+
+    public void ChangeTorsoLightPink()
+    {
+        torsoColor = 2;
+    }
+
+    public void ChangeTorsoSkin()
+    {
+        torsoColor = 3;
     }
 }
---
+++ New Unity Project/Assets/Scripts/Transition.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transition : MonoBehaviour
+{
+    public static Transition instance;
+
+
+    void Awake()
+
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this);
+        }
+        else
+        {
+            Destroy(this);
+        }
+    
+        
+    }
+}
