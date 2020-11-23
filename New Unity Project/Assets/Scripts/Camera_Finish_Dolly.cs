7ca3d7b770d86eee0f22351c0d9cc9113380dc44
loksangho <sangstar@gmail.com>
2020-11-23 12:51:36 +1100

    scene 1A (moxham exterior) added

---
+++ New Unity Project/Assets/Scripts/Camera_Finish_Dolly.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class Camera_Finish_Dolly : StateMachineBehaviour
+{
+    GameObject vcam; 
+    GameObject player;
+    
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        vcam = GameObject.Find("CM vcam1");
+        player = GameObject.Find("Player");
+        player.GetComponent<Platformer.Mechanics.PlayerController>().controlEnabled = false;
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        vcam.GetComponent<CinemachineVirtualCamera>().enabled = true; 
+        player.GetComponent<Platformer.Mechanics.PlayerController>().controlEnabled = true;
+
+    }
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
