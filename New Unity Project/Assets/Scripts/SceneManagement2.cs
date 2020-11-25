1fa8cf04b235e3ae218578036d64677423d73dfd
Pranav Manral <pranavmanral123@gmail.com>
2020-11-25 14:46:06 +1100

    loading and menu scenes

---
+++ New Unity Project/Assets/Scripts/SceneManagement2.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagement2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void SceneTransition()
+    {
+        SceneManager.LoadScene("Scene1");
+        
+    }
+}
